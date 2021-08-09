b3169ffae978df8278734c33bc969a869b16eed0
matthewsparr <matthewsparr247@gmail.com>
2021-08-09 16:58:49 -0400

    added traps, additional maps, changed asset directory

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from tqdm import tqdm
 
 wrapper = GymWrapperFactory()
 
-wrapper.build_gym_from_yaml('dbd', 'dbd.yaml', level=1)
+wrapper.build_gym_from_yaml('dbd', 'dbd.yaml', level=2)
 # Press the green button in the gutter to run the script.
 
 def random_direction():
@@ -22,51 +22,61 @@ def random_direction():
 def random_action(actions):
     return (np.random.choice(actions))
 
-killer_actions = [1,2,4,6]
-survivor_actions = [3,4,5,7,8]
+actions_list = ['initialize_killer', 'move', 'attack', 'recharge', 'damage_generator', 'place_trap', 'pick_up_trap', 'heal', 'repair_generator', 'disarm_trap', 'open_exit_gate']
+killer_actions = ['move', 'attack', 'recharge', 'damage_generator', 'place_trap', 'pick_up_trap']
+survivor_actions = ['move', 'heal', 'repair_generator', 'disarm_trap', 'open_exit_gate']
+killer_action_ids = [actions_list.index(i) for i in killer_actions]
+survivor_action_ids = [actions_list.index(i) for i in survivor_actions]
+
 if __name__ == '__main__':
-    env = gym.make('GDY-dbd-v0', global_observer_type=gd.ObserverType.SPRITE_2D) #  ISOMETRIC
+    env = gym.make('GDY-dbd-v0', global_observer_type=gd.ObserverType.SPRITE_2D, image_path='C:/Users/Matthew/Desktop/griddly/sprites') #  ISOMETRIC SPRITE_2D
     env.enable_history(True)
     env.reset()
     env.render(observer='global')
-    print(env.action_space)
 
-    # time.sleep(1)
-    # play(env, fps=30, zoom=3)
-    # Replace with your own control algorithm!
     generator_progress_bar = tqdm(total=100, leave=True, desc = 'generators', position=0)
     downed_survivor_progress_bar = tqdm(total=4, leave=True, desc = 'downed survivors', position=0)
+    exit_gate_progress_bar = tqdm(total=100, leave=True, desc='exit gate', position=0)
     monitor_generators = True
+    monitor_exit_gate = True
     monitor_survivors = False
+
     for s in range(10000):
-        time.sleep(0.01)
+        time.sleep(0.05)
         # actions = env.action_space.sample()
         actions = []
         for player in range(1,6):
             if player==1: ## killer
-                actions.append(np.array([random_action(killer_actions), random_direction()], dtype=np.int64))
+                actions.append(np.array([random_action(killer_action_ids), random_direction()], dtype=np.int64))
             else: ## survivor
-                actions.append(np.array([random_action(survivor_actions), random_direction()], dtype=np.int64))
-
+                actions.append(np.array([random_action(survivor_action_ids), random_direction()], dtype=np.int64))
+            # actions.append(np.array([1,4]))
         obs, reward, done, info = env.step(actions)
-
         state = env.get_state()
-        # print([i['Variables']['progress'] for i in state['Objects'] if i['Name']=='generator'])
-        progress = [i['Variables']['progress'] for i in state['Objects'] if i['Name']=='generator'][0]
-        if monitor_generators:
-            generator_progress_bar.n = progress
-            generator_progress_bar.refresh()
+        # if s>10:
+        #     print([i for i in state['Objects'] if i['Name']=='player'])
+        #     break
 
-        downed_survivors = state['GlobalVariables']['downed_survivors'][0]
-        if monitor_survivors:
-            downed_survivor_progress_bar.n = downed_survivors
-            downed_survivor_progress_bar.refresh()
+        ## progress bar stuff
+        if True:
 
-        exit_gate_progress = [i['Variables']['progress'] for i in state['Objects'] if i['Name']=='exit_gate'][0]
-        if exit_gate_progress > 0:
-            print(exit_gate_progress)
-        env.render(observer='global') # Renders the entire environment
+            progress = sum([i['Variables']['progress'] for i in state['Objects'] if i['Name']=='generator'])/3
+            if monitor_generators:
+                generator_progress_bar.n = progress
+                generator_progress_bar.refresh()
 
+            downed_survivors = state['GlobalVariables']['downed_survivors'][0]
+            if monitor_survivors:
+                downed_survivor_progress_bar.n = downed_survivors
+                downed_survivor_progress_bar.refresh()
+
+            if progress >= 100 and monitor_exit_gate:
+                exit_gate_progress = [i['Variables']['progress'] for i in state['Objects'] if i['Name']=='exit_gate'][0]
+                exit_gate_progress_bar.n = exit_gate_progress
+                exit_gate_progress_bar.refresh() ## progress bar handling
+
+        # env.render(observer='global') # Renders the entire environment
+        env.render(observer='global')
         if done:
             outcome = info['PlayerResults']['1']
             if outcome=='Win':
